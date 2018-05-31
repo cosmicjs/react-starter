ed6460425a31d2cdb7fb5eb259faae49a4a6da1d
Tony Spiro <tspiro@tonyspiro.com>
2018-05-30 20:37:16 -0500

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,5 @@
 export default () => (
 	<div className="footer">
-		&copy;{ new Date().getFullYear() } <a href="https://cosmicjs.com">Cosmic JS</a> React Starter
+		&copy;{ new Date().getFullYear() } <a href="https://cosmicjs.com">Cosmic JS</a> React Starter. <a href="https://github.com/cosmicjs/react-starter" target="_blank">View the code</a>.
 	</div>
 )
